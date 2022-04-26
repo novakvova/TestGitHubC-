12b54c310a552b6153490237b328fce05f1d51d8
novakvova <novakvova@gmail.com>
2022-04-26 20:04:49 +0300

    Add new line Console App

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitProject
         static void Main(string[] args)
         {
             Console.WriteLine("Привіт команда!");
+            Console.WriteLine("Сьогодні хороший день. Можна класно відтягнутися.");
         }
     }
 }
