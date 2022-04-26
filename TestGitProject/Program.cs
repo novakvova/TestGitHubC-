623c5bd00920043721e1b8b9a65d54fac5a3c785
novakvova <novakvova@gmail.com>
2022-04-26 19:16:08 +0300

    Initial Project

---
+++ TestGitProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привіт команда!");
+        }
+    }
+}
